eedff38d95c3c45803b6767ffb86193b079e166f
Miles Higbie <miles.higbie@gmail.com>
2020-02-22 13:17:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Add your code here
+
+const submitData = function(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name, 
+            email
+        })
+    })
+
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        const body = document.getElementsByTagName("body")[0]
+        body.innerHTML = object.id
+    })
+    .catch(function(error){
+        const body = document.getElementsByTagName("body")[0]
+        body.innerHTML = error.message
+    })
+}
+
+
+
+
+
+
+
+// function submitData(name, email) {
+//     return fetch('http://localhost:3000/users', {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name,
+//             email
+//         })
+//     })
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (object) {
+//             document.body.innerHTML = object["id"]
+//         })
+//         .catch(function (error) {
+//             document.body.innerHTML = error.message
+//         })
+// }
+
