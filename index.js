c2f98ec0fccb46cd9bd1eec0412d426c34bde02e
github username <github email address>
2020-02-22 19:28:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const submitData = (name, email) => {
+    const BASE_URL = 'http://localhost:3000/users'
+    const body = document.getElementsByTagName("body")[0]
+    const formData = {
+        name: name, 
+        email: email
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    
+    return fetch(BASE_URL, configObj)
+    .then(response => response.json())
+    .then(data => body.innerHTML = data.id)
+    .catch(error => body.innerHTML = error.message)
+}
+
